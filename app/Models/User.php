633bc9c8bdd9b431177b631a4af6d576a49736a4
leo8459 <92004716+leo8459@users.noreply.github.com>
2026-03-18 12:35:56 -0400

    TIKTOKEROS prueba

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * Alias de rol usado por varios modulos legacy de gestiones.
