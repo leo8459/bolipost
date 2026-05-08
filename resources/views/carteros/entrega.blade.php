76f19e76920e84f683a6f932fdafb3d90112a0c4
colee615 <94701865+colee615@users.noreply.github.com>
2026-05-08 13:02:06 -0400

    cambios

--- resources/views/carteros/entrega.blade.php
+++ resources/views/carteros/entrega.blade.php
@@ -8,7 +8,8 @@
     @php
         $canEntregaDeliver = (auth()->user()?->can('feature.carteros.entrega.deliver') ?? false)
             || (auth()->user()?->can('feature.carteros.cartero.deliver') ?? false);
-        $canEntregaAttempt = auth()->user()?->can('feature.carteros.entrega.attempt') ?? false;
+        $canEntregaAttempt = (auth()->user()?->can('feature.carteros.entrega.attempt') ?? false)
+            || (auth()->user()?->can('feature.carteros.cartero.deliver') ?? false);
         $forceCameraCapture = in_array($tipo_paquete, ['CONTRATO', 'EMS', 'SOLICITUD'], true);
     @endphp
     <div class="carteros-wrap entrega-wrap">
